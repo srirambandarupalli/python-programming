a65ec688ec2ea5b85f83a4167de2574775243b86
sarosaranya <37361091+sarosaranya@users.noreply.github.com>
2018-03-18 16:16:12 +0530

    Create largest of 3 numbers.py

---
+++ Beginner level/largest of 3 numbers.py
@@ -0,0 +1,9 @@
+x=input("enter the number:")
+y=input("enter the number:")
+z=input("enter the number:")
+if (x>y and x>z):
+    print("x is largest")
+elif(y>x and y>z):
+    print("y is largest")
+else:
+    print("z is largest")
